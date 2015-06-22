f7a7b4bbddd40b670e89faacca015bfe0e9e3e11
siriuslee <siriuslee305@gmail.com>
2015-06-21 21:15:54 -0700

    Expanded the script running portion of pecking_test.py

--- pyoperant/tlab/pecking_test.py
+++ pyoperant/tlab/pecking_test.py
@@ -23,6 +23,8 @@ class PeckingTest(GoNoGoInterrupt):
     # Additional configurations:
     # log_polling = (True / False)
     # log_polling_file = (filename)
+    # log_playback = (True / False)
+    # log_playback_file = (filename)
 
     def __init__(self, *args, **kwargs):
 
@@ -114,9 +116,10 @@ if __name__ == "__main__":
     import sys
 
     box_name = sys.argv[1]
+    config_dir = os.path.expanduser(os.path.join("~", "Dropbox", "pecking_test", "configs"))
 
     # Load config file
-    config_file = os.path.expanduser(os.path.join("~", "configs", "%s.yaml" % box_name))
+    config_file = os.path.join(config_dir, "%s.yaml" % box_name)
     if not os.path.exists(config_file):
         raise IOError("Config file does not exist: %s" % config_file)
 
@@ -124,6 +127,18 @@ if __name__ == "__main__":
         parameters = configure.ConfigureJSON.load(config_file)
     elif config_file.lower().endswith(".yaml"):
         parameters = configure.ConfigureYAML.load(config_file)
+
+    parameters["experiment_path"] = os.path.join(parameters["experiment_path"],
+                                                 parameters["subject"].name,
+                                                 dt.datetime.now().strftime("%d%m%y"))
+
+    if not os.path.exists(parameters["experiment_path"]):
+        os.makedirs(parameters["experiment_path"])
+
+    data_link = os.path.expanduser(os.path.join("~", "data_%s" % box_name))
+    if os.path.exists(data_link):
+        os.remove(data_link)
+    os.symlink(parameters["experiment_path"], data_link)
 
     # Create experiment object
     exp = PeckingTest(**parameters)
